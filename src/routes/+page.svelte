8d2dbacd3556cfaf6a0405fce05929b0008d212a
Augustus Bunce <augustusbunce@gmail.com>
2023-08-23 02:18:45 +0100

    google analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,10 @@
         <div class="w-2/4 space-y-8">
             <h1 class="h1 bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone">Promethium Network</h1>
             <h3 class="whitespace-normal font-semibold h3">Promethium Network is a Minecraft server network managed and owned by Shradinx and gusbunce. It was started in early July 2023 when Geographica, a previous Minecraft server, was announced to be shutting down. Below, you can find other information about the server, such as links to our rules document and our social medias.</h3>
-            <button type="button" class="btn variant-glass-primary" use:clipboard={ip} on:click={onClick} use:popup={popupHover}>play.promethium-network.net</button>
+            <div class="flex justify-between">
+                <button type="button" class="btn variant-glass-primary" use:clipboard={ip} on:click={onClick} use:popup={popupHover}>play.promethium-network.net</button>
+
+            </div>
         </div>
         <div>
             <iframe src="https://discord.com/widget?id=1125940812182724698&theme=dark" width="350" height="500" allowtransparency={true} frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" title="Discord Embed"/>
